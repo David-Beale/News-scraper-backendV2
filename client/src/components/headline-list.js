dfef2c53423922aaad9a85e0d36bd513b5627d62
joseph <jphctolentino@gmail.com>
2020-03-11 19:24:41 +0000

    feat: added circular image(loading) headline list

--- client/src/components/headline-list.js
+++ client/src/components/headline-list.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import Headline from './headline'
+import { CircularProgress } from '@material-ui/core';
 
 export default ({ headlines }) => {
   return (
     <div className="list__container">
       {headlines.length === 0 &&
         <div className="no-content-container">
-          <span className="no-content">No Headlines Yet!</span>
+          <CircularProgress />
         </div>}
 
       {headlines.length > 0 &&
