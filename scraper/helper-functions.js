ce64d2a299801684e57075aa2fda8eb6c675ff73
David B <david_beale@outlook.com>
2020-03-12 08:25:28 +0000

    changes to be deleted

--- scraper/helper-functions.js
+++ scraper/helper-functions.js
@@ -7,17 +7,6 @@ const SiteData = require("../server/models/site-data")
 
 let counter = 0;
 module.exports = {
-<<<<<<< HEAD
-	getHeadline: async function (link, test = false) {
-		if (test) return fs.readFileSync('./spec/bbc.html', 'utf8')
-		else {
-			try {
-				const response = await got(link);
-				const data = response.body;
-				// if (link === 'https://www.20minutos.es/') {
-				// 	return data
-				// }
-=======
   getHeadline: async function (link, test = false) {
     if (test) return fs.readFileSync('./spec/bbc.html', 'utf8')
     else {
@@ -65,7 +54,7 @@ module.exports = {
         headline = nextNode.children.filter(child => {
           return child.type === 'text' && child.data.trim().length > 5
         })[0].data.trim();
-        console.log('headline', headline)
+        // console.log('headline', headline)
       }
       if (summaryPath.length) {
         let targetNode = $(titleRoot)
@@ -91,7 +80,7 @@ module.exports = {
             }
           }
         }
-        console.log('summary', summary)
+        // console.log('summary', summary)
       }
       if (linkPath.length) {
         let targetNode = $(titleRoot)
@@ -103,7 +92,7 @@ module.exports = {
         const concatLink = website.match(regex);
         if (nextNode.attribs.href[0] === 'h') link = (nextNode.attribs.href)
         else link = (concatLink + nextNode.attribs.href)
-        console.log('link', link)
+        // console.log('link', link)
       }
       if (imagePath.length) {
         let targetNode = $(titleRoot)
@@ -112,9 +101,9 @@ module.exports = {
           nextNode = nextNode.children.filter(child => child.type === 'tag' || child.type === 'script' || child.type === 'style')[imagePath[i]]
         }
         image = nextNode.attribs[imageTag]
-        console.log('image', image)
+        // console.log('image', image)
       }
-      console.log('finished')
+      // console.log('finished')
       return { headline, summary, link, image };
     } catch (error) {
       return null
@@ -148,54 +137,4 @@ module.exports = {
     const siteData = await SiteData.find();
     return siteData
   }
-}
->>>>>>> 60dbf185e85535c3b9883aadb92b87b9319a698f
-
-				// fs.writeFileSync(`./${counter}.html`, data)
-				// console.log(counter, link)
-				counter++;
-				return data
-			} catch (error) {
-				console.log('get headline error', error.response.body[4]);
-			}
-		}
-	},
-	parseHeadline: function (data, selector) {
-		const $ = cheerio.load(`${data}`);
-		const headline = $(selector).first().text().trim();
-		return headline;
-	},
-	parseImage: function (data, imageSelector) {
-		const $ = cheerio.load(`${data}`);
-		const image = $(imageSelector).attr('src');
-		return image;
-	},
-	getDate: function () {
-		return [Number(moment(Date.now()).format("DD")), Number(moment(Date.now()).format("MM")),
-		Number(moment(Date.now()).format("YYYY")), moment(Date.now()).format("LT")]
-	},
-	saveData: async function (headlineArray, saveCounter, duplicateCounter, test = false) {
-		for (let i = 0; i < headlineArray.length; i++) {
-			if (test) headlineArray[i].hash = Date.now();
-			let bool = await Headlines.exists({ hash: headlineArray[i].hash })
-			if (bool) {
-				duplicateCounter++
-			}
-			else {
-				saveCounter++
-				headlineArray[i].save(function (err) {
-					if (err) throw err;
-				});
-			}
-			if (i === headlineArray.length - 1) {
-				console.log('ITEMS INSERTED : ', saveCounter)
-				console.log('DUPLICATE ITEMS : ', duplicateCounter)
-				return (`ITEMS INSERTED :  ${saveCounter} DUPLICATE ITEMS :  ${duplicateCounter}`)
-			}
-		}
-	},
-	getData: async function () {
-		const siteData = await SiteData.find();
-		return siteData
-	}
 }
