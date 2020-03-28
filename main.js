b90eadb01de871ffce0f393df0de31c11dd4ffcd
David B <david_beale@outlook.com>
2020-03-28 16:32:46 +0000

    remove front end

--- main.js
+++
@@ -1,8 +0,0 @@
-
-$(() => {
-  $( "object" ).on( "click", function() {
-    // e.preventDefault();
-    console.log( $( this ));
-  });
-})
-{/* <div>Hello There</div> */}
--- spec/test.js
+++
@@ -1,5 +0,0 @@
-const storeHeadlines = require("../scraper/store-headlines");
-const Headlines = require("../server/models/headlines")
-require("../server/db");
-
-storeHeadlines.store();
