bf2e3213b8b06fb590d833950901ea9f9f639f36
David B <david_beale@outlook.com>
2020-04-10 16:20:15 +0100

    test origin change

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const server = new ApolloServer({
 //CORS
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000',
+  origin: 'https://5e9088fd33ed246bc8715f88--db-newsfeeds.netlify.com/',
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
@@ -61,7 +61,7 @@ app.use('/users', require('./routes/users'));
 server.applyMiddleware({ 
   app,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://5e9088fd33ed246bc8715f88--db-newsfeeds.netlify.com/",
     credentials: true
   }, })
 
